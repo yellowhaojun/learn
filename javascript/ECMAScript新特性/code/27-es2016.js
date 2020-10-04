baf4bc1e0460a9f5ed3e6c1d223fc1e0c0430829
alex <yellowhaojun@gmail.com>
2020-10-04 21:50:40 +0800

    新增ES2016的笔记

---
+++ javascript/ECMAScript新特性/code/27-es2016.js
@@ -0,0 +1,8 @@
+// const arr = ['foo', 1, NaN, false]
+
+// console.log(arr.indexOf(NaN)) // -1
+// console.log(arr.includes(NaN)) // true
+
+
+console.log(Math.pow(2, 10)) // 1024
+console.log(2 ** 10) // 1024
