32877b3fcb959cb6958f27d138f00dfabe8f1abe
alex <yellowhaojun@gmail.com>
2020-11-09 21:51:02 +0800

    新增模块化代码

---
+++ 面试题/code/01.js
@@ -0,0 +1,25 @@
+// var a = []
+// for (var i = 0; i < 10; i++) {
+//   a[i] = function () {
+//     console.log(i)
+//   }
+// }
+// a[6]()
+
+
+// var tmp = 123
+// if (true) {
+//   console.log(tmp)
+//   let tmp
+// }
+
+// var a = 10
+// var obj = {
+//   a: 20,
+//   fn () {
+//     setTimeout(() => {
+//       console.log(this.a)
+//     })
+//   }
+// }
+// obj.fn()
